7b427ffdaf74036c164298a6dbbd4e5ffad6d770
Andrii Suhak <andreysygak911@gmail.com>
2026-05-20 18:07:34 +0300

    chore: android client bump version to 0.2.0 - alpha

--- airmouse-client-android/app/build.gradle.kts
+++ airmouse-client-android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.lagency.airmouse"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0 - alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
